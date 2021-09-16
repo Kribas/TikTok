5504e259ecb5cdeaf0cbc11883e4076d16546fae
Kribas Rimal <carlousrimal@gmail.com>
2021-09-16 10:48:24 +0545

    made changes

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpauuFpmGEA3kRURFCMbkKmC8hBIYFdAo",
+  authDomain: "tik-tok-clone-b0d6a.firebaseapp.com",
+  projectId: "tik-tok-clone-b0d6a",
+  storageBucket: "tik-tok-clone-b0d6a.appspot.com",
+  messagingSenderId: "191603491881",
+  appId: "1:191603491881:web:901fc2fe6ff4bcd9344838",
+};
+
+
